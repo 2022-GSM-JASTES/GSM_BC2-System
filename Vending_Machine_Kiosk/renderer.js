4bcb8ef25e2b075a137c9af3c88caa0caf632efc
rkadl9999 <rkadl9999@naver.com>
2022-12-20 19:32:43 +0900

    ✨| Refactoring code && IPC test

--- Vending_Machine_Kiosk/renderer.js
+++ Vending_Machine_Kiosk/renderer.js
@@ -1,17 +1,15 @@
-const {ipcRenderer} = require('electron');
+const { ipcRenderer } = require('electron');
 
 window.onload = function() {
-    function onClick() {
-        document.querySelector('.modal_frame').style.display = 'block';
-        document.getElementById('frame').style.background = 'rgba(177,177,177,1)';
-        document.getElementById('cnt').innerText = 1; //reset Cnt
-        totalPrice();
+    function totalPrice(){
+        let cnt = document.getElementById('cnt').innerText;
+
+        const price = 1500 * cnt;
+        const total_price = price.toLocaleString();
+
+        document.getElementById('total_price').innerText = "₩" + total_price; 
     }
-    function offClick() {
-        document.querySelector('.modal_frame').style.display = 'none';
-        document.getElementById('frame').style.background = 'rgba(255,255,255,1)';
-    }
-    function plusCnt(){
+    document.getElementById('plus').addEventListener('click',() => {
         const cnt = document.getElementById('cnt');
 
         let num = cnt.innerText;
@@ -23,8 +21,8 @@ window.onload = function() {
             cnt.innerText = num;
             totalPrice();
         }
-    }
-    function minusCnt(){
+    });
+    document.getElementById('minus').addEventListener('click', () => {
         const cnt = document.getElementById('cnt');
 
         let num = cnt.innerText;
@@ -36,17 +34,21 @@ window.onload = function() {
             cnt.innerText = num;
             totalPrice();
         }
-    }
-    function totalPrice(){
-        let cnt = document.getElementById('cnt').innerText;
-
-        const price = 1500 * cnt;
-        const total_price = price.toLocaleString();
-
-        document.getElementById('total_price').innerText = "₩" + total_price; 
-    }
-    document.getElementById('plus').addEventListener('click', plusCnt);
-    document.getElementById('minus').addEventListener('click', minusCnt);
-    document.getElementById('modal_btn').addEventListener('click', onClick);
-    document.getElementById('modal_close').addEventListener('click', offClick);
+    });
+    document.getElementById('modal_btn').addEventListener('click', () => {
+        document.querySelector('.modal_frame').style.display = 'block';
+        document.getElementById('frame').style.background = 'rgba(177,177,177,1)';
+        document.getElementById('cnt').innerText = 1; //reset Cnt
+        totalPrice();
+    });
+    document.getElementById('modal_close').addEventListener('click', () => {
+        document.querySelector('.modal_frame').style.display = 'none';
+        document.getElementById('frame').style.background = 'rgba(255,255,255,1)';
+    });
+    
+    document.getElementById('buy').addEventListener('click', () => {
+        let PayInfo = {'price' : document.getElementById('total_price').innerText,
+                        'cnt' : document.getElementById('cnt').innerText};
+        ipcRenderer.send('Pay', PayInfo);
+    })
 };
