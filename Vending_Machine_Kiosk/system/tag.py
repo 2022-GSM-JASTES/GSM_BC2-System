177a1927cd0c73bfb2e6059ceff739773c2a4914
rkadl9999 <rkadl9999@naver.com>
2022-12-22 20:46:58 +0900

    ✨| NFC tag data Hex to String

--- Vending_Machine_Kiosk/system/tag.py
+++ Vending_Machine_Kiosk/system/tag.py
@@ -2,7 +2,9 @@ import os
 
 try:
     hex_data = os.popen('nfc-emulate-forum-tag4').read() #NDEF read command
-    message = hex_data.split('\n')[24].split('55  ')[1] #NDEF에서 원하는 값만 인덱싱
+    message = hex_data.split('\n')[24].split('55')[1]#NDEF에서 원하는 값만 인덱싱
+    message = message.replace("  ","") #Remove a space
+    message = bytes.fromhex(message).decode('utf-8') #hex to string
     print(message)
 except:
     print('error')
