d674ba8d77b8da9a8ee5787276c533356bedf7b2
rkadl9999 <rkadl9999@naver.com>
2022-12-26 13:40:25 +0900

    ✨| Add NFC status Logic

--- Vending_Machine_Kiosk/renderer.js
+++ Vending_Machine_Kiosk/renderer.js
@@ -36,6 +36,10 @@ window.onload = function() {
         }
     });
     document.getElementById('modal_btn').addEventListener('click', () => {
+        document.querySelector('.NFC_reload').style.display = 'none';
+        document.querySelector('.NFC_ready').style.display = 'none';
+        document.querySelector('.No_money').style.display = 'none';
+        document.querySelector('.Pay_success').style.display = 'none';
         document.querySelector('.modal_frame').style.display = 'block';
         document.getElementById('frame').style.background = 'rgba(177,177,177,1)';
         document.getElementById('cnt').innerText = 1; //reset Cnt
@@ -47,12 +51,54 @@ window.onload = function() {
     });
     
     document.getElementById('buy').addEventListener('click', () => {
+        document.querySelector('.order').style.display = 'none';
+        document.querySelector('.NFC_ready').style.display = 'block';
+
         let PayInfo = {'price' : document.getElementById('total_price').innerText,
                         'cnt' : document.getElementById('cnt').innerText};
 
         ipcRenderer.send('Pay', PayInfo);
         ipcRenderer.on('res', (event, arg) => {
-            console.log(arg)
+            console.log(arg);
+
+            if(arg == 'error\n'){ //태그 오류
+                document.querySelector('.NFC_ready').style.display = 'none';
+                document.querySelector('.NFC_reload').style.display = 'block';
+                setTimeout(() => {
+                    location.href = "./index.html";
+                }, 5000);
+                setTimeout(() => {
+                    document.querySelector('.NFC_reload').style.display = 'none';
+                    document.querySelector('.modal_frame').style.display = 'none';
+                }, 10000);
+            }
+
+            else if(arg == 'no money'){ //돈 부족
+                document.querySelector('.NFC_ready').style.display = 'none';
+                document.querySelector('.NFC_reload').style.display = 'none';
+                document.querySelector('.No_money').style.display = 'block';
+                setTimeout(() => {
+                    location.href = "./index.html";
+                }, 5000);
+                setTimeout(() => {
+                    document.querySelector('.No_money').style.display = 'none';
+                    document.querySelector('.modal_frame').style.display = 'none';
+                }, 10000);
+            }
+
+            else{ //결제 성공
+                document.querySelector('.NFC_ready').style.display = 'none';
+                document.querySelector('.NFC_reload').style.display = 'none';
+                document.querySelector('.Pay_success').style.display = 'block';
+                setTimeout(() => {
+                    location.href = "./index.html";
+                }, 5000);
+                setTimeout(() => {
+                    document.querySelector('.Pay_success').style.display = 'none';
+                    document.querySelector('.modal_frame').style.display = 'none';
+                }, 10000);
+            }
+
         })
     })
 };
