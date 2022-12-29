0d735ae071ef095f2b84fe00682936fee651bb30
rkadl9999 <rkadl9999@naver.com>
2022-12-29 14:52:30 +0900

    ✨| Change conditional statement structure

--- Vending_Machine_Kiosk/main.js
+++ Vending_Machine_Kiosk/main.js
@@ -7,59 +7,62 @@ const fetch = require('electron-fetch').default;
 ipcMain.on('Pay',(event, arg) => {
     console.log(arg);
     exec('python ./system/tag.py', (err,out,stderr) => {
-        out = out.replace(/\n/g, ""); //개행문자 제거            
+        out = out.replace(/\n/g, ""); //개행문자 제거          
 
         if(out == 'error')
             event.sender.send('res', 'error')
-        else if(out != 'error' && price > money.account) //잔액 부족 로직
-            event.sender.send('res', 'no money');
         else{
             let price = parseInt(arg.price.replace(",","").replace("₩","")); //원화 표시 제거
             let money = JSON.parse(out);
 
-            let account = money.account - price;
-
-            const block = {
-                "email" : money.email,
-                "balance" : account,
-                "menu" : arg.menu,
-                "price" : price,
-                "quantity" : Number(arg.cnt)
-            };
-
-            const data = {
-              method: 'POST',
-              headers: {
-                'Content-Type': 'application/json',
-                'Accept': 'text/event-stream'
-              },
-              //body: block
-              body: `{"email" : "${money.email}","balance" : ${account},"menu" : "${arg.menu}","price" : ${price},"quantity" : ${arg.cnt}}`
-            };
-            console.log(data)
-            fetch('http://10.82.20.0:3000/send', data);
-
-            console.log(block);
-            event.sender.send('res', out);
-
-            let menuNo;
-
-            switch (arg.menu) {
-                case '케이준:눈을 감자':
-                    menuNo = 1;
-                    break;
-                case '아폴로':
-                    menuNo = 2;
-                    break;
-                case '츄파춥스 사워 크롤러':
-                    menuNo = 3;
-                    break;
-                default:
-                    break;
+            if(price > money.account)
+                event.sender.send('res', 'no money')
+            else{
+                let account = money.account - price;
+    
+                const block = {
+                    "email" : money.email,
+                    "balance" : account,
+                    "menu" : arg.menu,
+                    "price" : price,
+                    "quantity" : Number(arg.cnt)
+                };
+    
+                const data = {
+                    method: 'POST',
+                    headers: {
+                    'Content-Type': 'application/json',
+                    'Accept': 'text/event-stream'
+                    },
+                    //body: block
+                    body: `{"email" : "${money.email}","balance" : ${account},"menu" : "${arg.menu}","price" : ${price},"quantity" : ${arg.cnt}}`
+                };
+                console.log(data)
+                fetch('http://10.82.20.0:3000/send', data);
+    
+                console.log(block);
+                event.sender.send('res', out);
+    
+                let menuNo;
+    
+                switch (arg.menu) {
+                    case '케이준:눈을 감자':
+                        menuNo = 1;
+                        break;
+                    case '아폴로':
+                        menuNo = 2;
+                        break;
+                    case '츄파춥스 사워 크롤러':
+                        menuNo = 3;
+                        break;
+                    default:
+                        break;
+                }
+    
+                exec('python ./system/servo.py ' + menuNo + ' ' + arg.cnt);
             }
-
-            exec('python ./system/servo.py ' + menuNo + ' ' + arg.cnt);
         }
+        
     })
 
 })
